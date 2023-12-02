43bdb66d0fad48dd31888edd8096789014656e58
khang <khang.job@gmail.com>
2023-12-02 20:23:20 +0700

    add annotate gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  image      :string
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Product < ApplicationRecord
 
 end
