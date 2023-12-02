1b666ce00d1c08e7841b6f95eb25f14c7f83df0e
khang <khang.job@gmail.com>
2023-12-02 22:23:24 +0700

    add product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,5 +10,24 @@
 #  updated_at :datetime         not null
 #
 class Product < ApplicationRecord
+  has_one_attached :image do |attachable|
+    attachable.variant :thumbnail, resize_to_limit: [100, 100]
+  end
 
+  enum status: {
+    created: 'Created',
+    reviewing: 'In review',
+    approved: 'Approved',
+    deny: 'Deny',
+    published: 'Published',
+    limited: 'Limited'
+  }
+
+  validates :name, :status, presence: true
+  validates :name, length: { minimum: 5, maximum: 255 }
+  validates :status, inclusion: { in: statuses.keys }
+
+  def thumb_url
+    Rails.application.routes.url_helpers.rails_representation_url(image.variant(:thumbnail).processed, only_path: true)
+  end
 end
